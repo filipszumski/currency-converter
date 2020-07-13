23b706b0739be326df885a4d334f7f22960f3550
Filip Szumski <filipszumski@gmail.com>
2020-07-14 01:26:12 +0200

    added functions to js

--- js/script.js
+++ js/script.js
@@ -1,68 +1,105 @@
-console.log("Hello World");
+{
+    const welcome = () => {
+        console.log("Hello World");
+    }
 
-let formElement = document.querySelector(".js-form");
-let amountRecivedElement = document.querySelector(".js-amountRecived");
-let moneyAmountElement = document.querySelector(".js-moneyAmount")
-let possessedCurrencyElement = document.querySelector(".js-possessedCurrency");
-let wantedCurrencyElement = document.querySelector(".js-wantedCurrency")
-let currencyRateElement = document.querySelector(".js-currencyRate")
+    const displayAmountInConsole = () => {
+        const moneyAmountElement = document.querySelector(".js-moneyAmount")
+        console.log(`Aktualna kwota: ${moneyAmountElement.value}`);
+    }
 
-let plnRate = 1;
-let eurRate = 4.4660;
-let usdRate = 3.9806;
-let gbpRate = 4.8851;
-let chfRate = 4.1818;
+    const calculatePossessedCurrencyAmount = (possessedCurrency, amount) => {
+        const plnRate = 1;
+        const eurRate = 4.4660;
+        const usdRate = 3.9806;
+        const gbpRate = 4.8851;
+        const chfRate = 4.1818;
 
-moneyAmountElement.addEventListener("input", () => {
-    console.log(`Aktualna kwota: ${moneyAmountElement.value}`);
-})
+        switch (possessedCurrency) {
+            case "pln":
+                return amount * plnRate
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+            case "eur":
+                return amount * eurRate
 
-    let value1;
-    let value2;
-    let amount = +moneyAmountElement.value;
-    let possessedCurrency = possessedCurrencyElement.value;
-    let wantedCurrency = wantedCurrencyElement.value;
+            case "usd":
+                return amount * usdRate
 
-    switch (possessedCurrency) {
-        case "pln":
-            value1 = amount * plnRate
-            break;
-        case "eur":
-            value1 = amount * eurRate
-            break;
-        case "usd":
-            value1 = amount * usdRate
-            break;
-        case "gbp":
-            value1 = amount * gbpRate
-            break;
-        case "chf":
-            value1 = amount * chfRate
-            break;
+            case "gbp":
+                return amount * gbpRate
+
+            case "chf":
+                return amount * chfRate
+        }
+    }
+
+    const calculateWantedCurrencyAmount = (wantedCurrency, result) => {
+        const plnRate = 1;
+        const eurRate = 4.4660;
+        const usdRate = 3.9806;
+        const gbpRate = 4.8851;
+        const chfRate = 4.1818;
+
+        switch (wantedCurrency) {
+            case "pln":
+                return result / plnRate
+
+            case "eur":
+                return result / eurRate
+
+            case "usd":
+                return result / usdRate
+
+            case "gbp":
+                return result / gbpRate
+
+            case "chf":
+                return result / chfRate
+        }
+    }
+
+    const displayResult = (result1) => {
+        const amountRecivedElement = document.querySelector(".js-amountRecived");
+
+        amountRecivedElement.value = result1.toFixed(2)
+    }
+
+    const displaycurrencyIndex = (amount, result1) => {
+        const currencyRateElement = document.querySelector(".js-currencyRate")
+
+        const currencyIndex = amount / result1;
+        currencyRateElement.value = currencyIndex.toFixed(2)
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const moneyAmountElement = document.querySelector(".js-moneyAmount")
+        const possessedCurrencyElement = document.querySelector(".js-possessedCurrency");
+        const wantedCurrencyElement = document.querySelector(".js-wantedCurrency")
+
+
+        const amount = +moneyAmountElement.value;
+        const possessedCurrency = possessedCurrencyElement.value;
+        const wantedCurrency = wantedCurrencyElement.value;
+
+        const result = calculatePossessedCurrencyAmount(possessedCurrency, amount);
+        const result1 = calculateWantedCurrencyAmount(wantedCurrency, result)
+
+        displayResult(result1);
+        displaycurrencyIndex(amount, result1)
 
     }
-    switch (wantedCurrency) {
-        case "pln":
-            value2 = value1 / plnRate
-            break;
-        case "eur":
-            value2 = value1 / eurRate
-            break;
-        case "usd":
-            value2 = value1 / usdRate
-            break;
-        case "gbp":
-            value2 = value1 / gbpRate
-            break;
-        case "chf":
-            value2 = value1 / chfRate
-            break;
-    }
-    amountRecivedElement.value = value2.toFixed(2)
-    let value3 = amount / value2;
-    currencyRateElement.value = value3.toFixed(4)
-})
 
+    const init = () => {
+        const moneyAmountElement = document.querySelector(".js-moneyAmount")
+        moneyAmountElement.addEventListener("input", displayAmountInConsole)
+
+        welcome();
+
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit)
+    }
+    init();
+}
